fdfaa0e6500b9f5b1ae6cde10a3c7a7b92662f11
Austin Hoang <austinhoang14@gmail.com>
2023-02-15 22:52:20 -0500

    will sentence ext done

--- popup.js
+++ popup.js
@@ -1,14 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const dialogBox = document.getElementById('dialog-box');
+  const text = document.querySelector('a');
+  const dialog = document.querySelector('p');
   const query = { active: true, currentWindow: true };
 
   chrome.tabs.query(query, (tabs) => {
-    dialogBox.innerHTML = `What are you doing on ${tabs[0].title}? Get back to <a href="https://leetcode.com/">work</a>!`
-    const anchors = document.querySelectorAll('a');
-    anchors.forEach(anchor => {
-      const location = anchor.getAttribute('href');
-      anchor.addEventListener('click', () => chrome.tabs.create({ active: true, url: location }));
-    })
+    const links = [
+      'https://leetcode.com/',
+      'https://csx.codesmith.io/',
+      'https://www.codewars.com/',
+      'https://www.freecodecamp.org/learn/coding-interview-prep/',
+      'https://www.structy.net/',
+      'https://www.patterns.dev/'
+    ]
+
+    // for (const link of links) {
+    // text.innerHTML = `<a id="text" href="${links[Math.floor(Math.random() * links.length)]}">work</a>`
+    const anchor = document.querySelector('a');
+    anchor.setAttribute('href', `${links[Math.floor(Math.random() * links.length)]}`)
+
+    dialog.innerHTML = `What are you doing on ${tabs[0].title}?`
+    // }
+
+    // const container = document.getElementById('container');
+    // container.appendChild(anchor);
+
+    const location = anchor.getAttribute('href');
+    anchor.addEventListener('click', () => chrome.tabs.create({ active: true, url: location }));
   });
 
 
